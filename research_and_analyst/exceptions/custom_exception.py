4617f2ca851ee949942e2dcce64b5abc87e2112f
Emmanuel <ajalae2@gmail.com>
2026-03-03 14:13:20 -0100

    custom exception created

--- research_and_analyst/exceptions/custom_exception.py
+++ research_and_analyst/exceptions/custom_exception.py
@@ -1,56 +1,57 @@
-import os
-import logging
-from datetime import datetime
-import structlog
+import traceback
+from typing import Optional, cast
+import sys
 
-class CustomLogger:
-    def __init__(self, log_dir="logs"):
-        #Ensure log directory exists
-        self.logs_dir = os.path.join(os.getcwd(), log_dir)
-        os.makedirs(self.logs_dir, exist_ok=True)
+class ResearchAnalystException(Exception):
+    def __init__(self, error_message, error_details: Optional[object]=None):
+        if isinstance(error_message, BaseException):
+            norm_arg = str(error_message)
 
-        #Timestamped log file (for persistence)
-        log_file = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-        self.log_file_path = os.path.join(self.logs_dir, log_file)
+        else:
+            norm_arg = str(error_message)
 
-    def get_logger(self, name = __file__):
-        logger_name = os.path.basename(name)
+        exc_type = exc_value = exc_tb = None
+        if error_details is None:
+            exc_type, exc_value, exc_tb = sys.exec_info()
+        
+        else:
+            if hasattr(error_details, 'exc_info'):
+                exc_info_obj = cast(sys, error_details)
+                exc_type, exc_value, exc_tb = exc_info_obj.exc_info()
+            
+            elif isinstance(error_details, BaseException):
+                exc_type, exc_value, exc_tb = type(error_details), error_details, error_details.__traceback__
 
-        # Configure logging for console + file(Both JSON)
-        file_handler = logging.FileHandler(self.log_file_path)
-        file_handler.setLevel(logging.INFO)
-        file_handler.setFormatter(logging.Formatter("%{message}s%"))
+            else:
+                exc_type, exc_value, exc_tb = sys.exc_info()
+        
+        last_tb = exc_tb
+        while last_tb and last_tb.tb_next:
+            last_tb = last_tb.tb.next
 
-        console_handler = logging.StreamHandler()
-        console_handler.setLevel(logging.INFO)
-        console_handler.setFormatter(logging.Formatter("%{message}s"))
+        self.file_name = last_tb.tb.frame.f_code.co_filename if last_tb else "<unknown>"
+        self.lineno = last_tb.tb.lineno if last_tb else -1
+        self.error_message = norm_arg
 
-        logging.basicConfig(
-            level = logging.INFO,
-            format= "%{messages}s",
-            handlers=[file_handler, console_handler]
-        )
+        if exc_type and exc_tb:
+            self.tracback_str = "".join(traceback.format_exception(exc_type, exc_value, exc_tb))
+        else:
+            self.traceback_str = ""
+        
+        super().__init__(self.__str__())
 
-        ## configuring structing for basic JSON structuring
-        structlog.configure(
-            processors=[
-                structlog.processors.TimeStamper(
-                    fmt= 'iso',
-                    utc= True,
-                    key = 'timestamp'
-                ),
-                structlog.processors.add_loglevel,
-                structlog.processors.EventRunner(to='event'),
-                structlog.processors.JSONRenderer()
-            ],
+    def __str__(self):
+        base = f"Eror in [{self.file_name}] at line [{self.lineno}] | Message: {self.error_message}"
+        if self.traceback_str:
+            return f"{base}\nTraceback:\n{self.traceback_str}"
+        
+        return base
 
-            logger_factory = structlog.stdlib.LoggerFactory(),
-            cache_logger_on_first_use=True,
-        )
-
-        return structlog.get_logger(logger_name)
+    def __repr__(self):
+        return f"ResearcAnalystException(file={self.file_name|r}, line={self.lineno}, message = {self.error_message})"
     
-if __name__ == "__main__":
-    logger = CustomLogger().get_logger(__file__)
-    logger.info("User upload a file", user_id=123, filename="report.pdf")
-    logger.error('Failed to precess PDF', error="File not found", user_id=123)
+if __name__ =="__main__":
+    try:
+        a = 1/0
+    except Exception as e:
+        raise ResearchAnalystException("Division failed", e ) from e
